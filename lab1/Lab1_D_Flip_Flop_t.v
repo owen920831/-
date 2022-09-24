ad3f4becdb29a4e500299b95180564456b2c4fb0
owen920831 <owen920831@gmail.com>
2022-09-24 19:51:18 +0800

    lab1

--- lab1/Lab1_D_Flip_Flop_t.v
+++ lab1/Lab1_D_Flip_Flop_t.v
@@ -1,35 +1,33 @@
 `timescale 1ns/1ps
-module Toggle_Flip_Flop_t;
+
+module D_Flip_Flop_t;
+
 // input and output signals
 reg clk = 1'b0;
-reg t = 1'b0;
-reg rst_n = 1'b0;
+reg d = 1'b0;
 wire q;
+
 // generate clk
 always#(1) clk = ~clk;
-always#(4) rst_n = ~rst_n;
+
 // test instance instantiation
-Toggle_Flip_Flop TFF(
+D_Flip_Flop DFF(
     .clk(clk),
-    .q(q),
-    .rst_n(rst_n),
-    .t(t)
+    .d(d),
+    .q(q)
 );
+
 // uncommment and add "+access+r" to your nverilog command to dump fsdb waveform on NTHUCAD
 // initial begin
 //      $fsdbDumpfile("DFF.fsdb");
 //      $fsdbDumpvars;
 // end
+
 // brute force 
 initial begin
-    @(negedge clk) t = 1'b1;
-    @(negedge clk) t = 1'b0;
-    @(negedge clk) t = 1'b1;
-    @(negedge clk) t = 1'b0;
-    @(negedge clk) t = 1'b1;
-    @(negedge clk) t = 1'b0;
-    @(negedge clk) t = 1'b1;
-    @(negedge clk) t = 1'b0;
+    @(negedge clk) d = 1'b1;
+    @(negedge clk) d = 1'b0;
     @(negedge clk) $finish;
 end
+
 endmodule
