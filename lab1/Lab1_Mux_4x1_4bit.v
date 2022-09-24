c350b49a3c4f542ef3515098f668c918c1d44da9
owen920831 <owen920831@gmail.com>
2022-09-25 00:25:04 +0800

    lab1

--- lab1/Lab1_Mux_4x1_4bit.v
+++ lab1/Lab1_Mux_4x1_4bit.v
@@ -1,38 +1,37 @@
 `timescale 1ns/1ps
 
 module Mux_4x1_4bit(a, b, c, d, sel, f);
-input [3:0] a, b, c, d;
-input [1:0] sel;
-output [3:0] f;
-wire [3:0] x, y;
+    input [3:0] a, b, c, d;
+    input [1:0] sel;
+    output [3:0] f;
+    wire [3:0] x, y;
 
-Mux_2x1_4bit M0(a, b, sel[0], x);
-Mux_2x1_4bit M1(c, d, sel[0], y);
-Mux_2x1_4bit M2(x, y, sel[1], f);
+    Mux_2x1_4bit M0(a, b, sel[0], x);
+    Mux_2x1_4bit M1(c, d, sel[0], y);
+    Mux_2x1_4bit M2(x, y, sel[1], f);
 
 endmodule
 
 module Mux_2x1_4bit(a, b, sel, o);
-input [3:0] a, b;
-input sel;
-output [3:0] o;
-wire _sel;
-wire [3:0] A, B;
+    input [3:0] a, b;
+    input sel;
+    output [3:0] o;
+    wire _sel;
+    wire [3:0] A, B;
 
-not n0(_sel, sel);
+    not n0(_sel, sel);
 
-and a0(A[0], a[0], _sel);
-and a1(A[1], a[1], _sel);
-and a2(A[2], a[2], _sel);
-and a3(A[3], a[3], _sel);
-and b0(B[0], b[0], sel);
-and b1(B[1], b[1], sel);
-and b2(B[2], b[2], sel);
-and b3(B[3], b[3], sel);
+    and a0(A[0], a[0], _sel);
+    and a1(A[1], a[1], _sel);
+    and a2(A[2], a[2], _sel);
+    and a3(A[3], a[3], _sel);
+    and b0(B[0], b[0], sel);
+    and b1(B[1], b[1], sel);
+    and b2(B[2], b[2], sel);
+    and b3(B[3], b[3], sel);
 
-or g0(o[0], A[0], B[0]);
-or g1(o[1], A[1], B[1]);
-or g2(o[2], A[2], B[2]);
-or g3(o[3], A[3], B[3]);
-
+    or g0(o[0], A[0], B[0]);
+    or g1(o[1], A[1], B[1]);
+    or g2(o[2], A[2], B[2]);
+    or g3(o[3], A[3], B[3]);
 endmodule
--- lab1/Lab1_TeamX_Dmux_1x4_4bit_t.v
+++ lab1/Lab1_TeamX_Dmux_1x4_4bit_t.v
@@ -1,35 +1,35 @@
 `timescale 1ns/1ps
 
 module Dmux_1x4_4bit_t;
-wire [3:0]a;
-wire [3:0]b;
-wire [3:0]c;
-wire [3:0]d;
-reg [1:0]sel = 2'b0;
-reg [3:0]in = 4'b0000;
+    wire [3:0]a;
+    wire [3:0]b;
+    wire [3:0]c;
+    wire [3:0]d;
+    reg [1:0]sel = 2'b0;
+    reg [3:0]in = 4'b0000;
 
-Dmux_1x4_4bit m1(
-    .in (in),
-    .a (a),
-    .b (b),
-    .c (c),
-    .d (d),
-    .sel (sel)
-    
-);
-// uncommment and add "+access+r" to your nverilog command to dump fsdb waveform on NTHUCAD
-// initial begin
-//      $fsdbDumpfile("MUX.fsdb");
-//      $fsdbDumpvars;
-// end
+    Dmux_1x4_4bit m1(
+        .in (in),
+        .a (a),
+        .b (b),
+        .c (c),
+        .d (d),
+        .sel (sel)
+        
+    );
+    // uncommment and add "+access+r" to your nverilog command to dump fsdb waveform on NTHUCAD
+    // initial begin
+    //      $fsdbDumpfile("MUX.fsdb");
+    //      $fsdbDumpvars;
+    // end
 
-initial begin
-    repeat (8)begin
-        #1 sel = sel + 2'b1;
-        repeat (2 ** 4) begin
-            #1 in = in + 4'b1;
+    initial begin
+        repeat (8)begin
+            #1 sel = sel + 2'b1;
+            repeat (2 ** 4) begin
+                #1 in = in + 4'b1;
+            end
         end
+        #1 $finish;
     end
-    #1 $finish;
-end
 endmodule
