43f630993f0c04388a27ebeb641aeff58a37ad29
owen920831 <owen920831@gmail.com>
2022-09-27 15:55:01 +0800

    1

--- lab1/Lab1_TeamX_Crossbar_2x2_4bit.v
+++ lab1/Lab1_TeamX_Crossbar_2x2_4bit.v
@@ -10,6 +10,6 @@ module Crossbar_2x2_4bit(in1, in2, control, out1, out2);
     not n1(_control, control);
     Dmux_1x2_4bit in1_Dmux(in1, in1_0, in1_1, control);
     Dmux_1x2_4bit in2_Dmux(in2, in2_0, in2_1, _control);
-    Mux_2x1_4bit out1_mux(in1_0, in1_1, control, out1);
-    Mux_2x1_4bit out2_mux(in2_0, in2_1, _control, out2);
+    Mux_2x1_4bit out1_mux(in1_0, in2_0, control, out1);
+    Mux_2x1_4bit out2_mux(in1_1, in2_1, _control, out2);
 endmodule
