5b5a2654c7365a7d08a0e697f76cbc53f3dfec2e
owen920831 <owen920831@gmail.com>
2022-09-29 18:42:06 +0800

    2

--- lab2/Lab2_Majority.v
+++ lab2/Lab2_Majority.v
@@ -1,7 +1,35 @@
 `timescale 1ns/1ps
 
-module Majority(a, b, c, out);
-input a, b, c;
-output out;
+module AND(a, b, out);
+    input a, b;
+    output out;
+    wire ab;
+    nand n1(ab, a, b);
+    nand n2(out, ab, ab);
+endmodule
 
+module OR(a, b, out);
+    input a, b;
+    output out;
+    wire _a, _b;
+    NOT n1(a, _a);
+    NOT n2(b, _b);
+    nand n3(out, _a, _b);
+endmodule
+
+module NOT(a, out);
+    input a;
+    output out;
+    nand n1(out, a, a);
+endmodule
+
+module Majority(a, b, c, out);
+    input a, b, c;
+    output out;
+    wire w1, w2, w3, w4;
+    AND a1(a, b, w1);
+    AND a2(b, c, w4);
+    AND a3(a, c, w2);
+    OR o1(w1, w2, w3);
+    OR o2(w3, w4, out);
 endmodule
