4658e9996bf7f717147c9832eaaf7967cf9d9cd2
khris shie <winethh@gmail.com>
2022-10-17 00:50:09 +0800

    adv1 done

---
+++ lab3/Lab3_TeamX_Ping_Pong_Counter.v
@@ -0,0 +1,31 @@
+`timescale 1ns/1ps
+
+module Ping_Pong_Counter (clk, rst_n, enable, direction, out);
+input clk, rst_n;
+input enable;
+output direction;
+output [4-1:0] out;
+
+reg [3:0] out;
+reg direction = 1'b1;
+always @(posedge clk) begin
+    if (!rst_n) begin
+        out <= 4'b0000;
+        direction = 1'b1;
+    end
+    else if (enable) begin
+        if (direction) begin
+            if (out > 4'b1110)
+                out <= 4'b1110;
+            else out <= out + 4'b0001;
+            direction <= (out == 4'b1111) ? 0 : 1;
+        end
+        else begin
+            if (out < 4'b0001)
+                out <= 4'b0001;
+            else out <= out - 4'b0001;
+            direction <= (out == 4'b0000) ? 1 : 0;
+        end
+    end
+end
+endmodule
